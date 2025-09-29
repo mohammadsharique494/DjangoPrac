8f81f2d7b09367e06e4de89a11c81078f0a3e75c
Oreilly Lab <lab@oreilly.com>
2025-09-29 06:32:24 +0000

    added templates

--- StudyBud/StudyBud/settings.py
+++ StudyBud/StudyBud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9%!juaijq&@13aoudv87cdnna0kmwoaq$b!%vm-9jpd4=k+p@c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['32dedd77570e406e8518dce5229770b0-2886733829-23000-host09nc.environments.katacoda.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'StudyBud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
