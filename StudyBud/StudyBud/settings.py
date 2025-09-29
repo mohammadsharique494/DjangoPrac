67d84d73aa2dff4666f0cd412f5fc7e73961eaf4
Oreilly Lab <lab@oreilly.com>
2025-09-29 07:25:47 +0000

    base templated addded

--- StudyBud/StudyBud/settings.py
+++ StudyBud/StudyBud/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-9%!juaijq&@13aoudv87cdnna0kmwoaq$b!%vm-9jpd4=k+p@c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['32dedd77570e406e8518dce5229770b0-2886733829-23000-host09nc.environments.katacoda.com']
+ALLOWED_HOSTS = ['32dedd77570e406e8518dce5229770b0-2886733829-23000-host09nc.environments.katacoda.com',
+'2886732805-23000-host22nc.environments.katacoda.com']
 
 
 # Application definition
--- StudyBud/base/views.py
+++ StudyBud/base/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+rooms = [
+    {'id':1 , 'name':'let learns python'},
+    {'id':2 , 'name':'Springboot is fun'},
+    {'id':3 , 'name':'lets build api with Django'},
+    {'id':4 , 'name':'Deployment'},
+    
+]
 # Create your views here.
 def home(request):
-    return render(request , 'home.html')
+    context ={'rooms':rooms}
+    return render(request , 'base/home.html', context)
 def room(request):
     return render(request,'room.html')
